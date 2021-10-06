88578fba3ffabe11638c3dd1e05fa383737f48d3
CatOnXTC <karolsienkiewicz03@gmail.com>
2021-10-06 10:03:48 +0200

    initiate HP

--- Path Fighters/Assets/Scripts/unitScript.cs
+++ Path Fighters/Assets/Scripts/unitScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class unitScript : MonoBehaviour
 {
     public float speed;
+    public int hitPoints;
+    private string status;
 
     private float rayDistance = 0.03f;
     private LayerMask ignoreMask;
@@ -13,16 +15,24 @@ public class unitScript : MonoBehaviour
     void Start()
     {
         ignoreMask = LayerMask.GetMask("Path");
+        this.status = "Moving";
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (!Physics2D.Raycast(new Vector2(this.transform.position.x + 0.11f, this.transform.position.y), new Vector2(1f, 0f), rayDistance, ~ignoreMask) && this.transform.position.x < 6)
+        if (this.hitPoints <= 0)
         {
-            this.transform.position += new Vector3(this.speed * Time.deltaTime, 0, 0);
+            this.status = "Dying";
         }
-        Debug.DrawLine(new Vector2(this.transform.position.x + 0.11f, this.transform.position.y), new Vector2(this.transform.position.x+0.14f, this.transform.position.y), Color.green);
+
+        if (this.status != "Dying")
+        {
+            if (!Physics2D.Raycast(new Vector2(this.transform.position.x + 0.11f, this.transform.position.y), new Vector2(1f, 0f), rayDistance, ~ignoreMask) && this.transform.position.x < 6)
+            {
+                this.transform.position += new Vector3(this.speed * Time.deltaTime, 0, 0);
+            }
+            Debug.DrawLine(new Vector2(this.transform.position.x + 0.11f, this.transform.position.y), new Vector2(this.transform.position.x + 0.14f, this.transform.position.y), Color.green);
+        }
     }
 }
