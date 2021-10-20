eea5eb0d7b3cc3c2aabcd8a6b5fafe5276128fdd
CatOnXTC <karolsienkiewicz03@gmail.com>
2021-10-20 19:13:58 +0200

    health bars added
    Now one can see how much HP the soldiers have

--- Path Fighters/Assets/Scripts/unitScript.cs
+++ Path Fighters/Assets/Scripts/unitScript.cs
@@ -160,7 +160,7 @@ public class unitScript : MonoBehaviour
         if (this.hitPoints <= 0)
         {
             this.state = State.Dying;
-            Destroy(gameObject, 0.5f);
+            Destroy(gameObject, 0.1f);
         }
     }
 }
