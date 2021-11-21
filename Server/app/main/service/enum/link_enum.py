f3689f5d4b48010f1b6d99a608d5455829cbf3b5
96peterharris <96peterharris@gmail.com>
2021-11-21 19:33:46 +0100

    Improvement in ENUM

--- Server/app/main/service/enum/link_enum.py
+++ Server/app/main/service/enum/link_enum.py
@@ -2,10 +2,11 @@ from enum import Enum
 
 class LinkEnum(Enum): # HERE WE SHOULD CHANGE LINKS TO APPROPRIET VERSION ON DEPLOY
     """ Enumerate class which store links information. """
-    API_ALL_USERS = "http://127.0.0.1:5000/users" 
-    API_USER_BY_USERNAME = "http://127.0.0.1:5000/users/{}"
-    API_USER_BY_ID = "http://127.0.0.1:5000/users/id/{}"
-    API_ALL_STATS= "http://127.0.0.1:5000/stats"
+    API_URL = "http://127.0.0.1:5000"
+    API_ALL_USERS = API_URL + "/users" 
+    API_USER_BY_USERNAME = API_URL + "/users/{}"
+    API_USER_BY_ID = API_URL + "/users/id/{}"
+    API_ALL_STATS= API_URL + "/stats"
     API_STATS_BY_ID = API_ALL_STATS + "/{}"
     API_STATS_ADD_WIN = API_STATS_BY_ID + "/add-win"
     API_STATS_ADD_FAIL = API_STATS_BY_ID + "/add-fail"
