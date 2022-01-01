293baf34cda1496d79a129c8a4f050bd502bd786
sulkowski99 <sulkowski99@gmail.com>
2022-01-01 20:41:44 +0100

    Init Popup

---
+++ Path Fighters/Assets/Scripts/RegisterAccount.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterAccount : MonoBehaviour
+{
+    public Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Fail();
+    }
+    void Fail()
+    {
+        
+    }
+    void Success()
+    {
+
+    }
+}
