3e4099c24724bcdc2b7ee66bc990906a9b2691c3
sulkowski99 <56975114+sulkowski99@users.noreply.github.com>
2022-01-04 09:30:50 +0100

    Game data init

---
+++ Path Fighters/Assets/Scripts/updateGameParameters.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class updateGameParameters : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _UserHP;
+    [SerializeField] TextMeshProUGUI _EnemyHP;
+    [SerializeField] TextMeshProUGUI _UserGold;
+    [SerializeField] TextMeshProUGUI _Research;
+    // Start is called before the first frame update
+    void UpdateGold()
+    {
+        
+    }
+    void UpdateEnemyHP()
+    {
+
+    }
+    void UpdateUserHP()
+    {
+
+    }
+    void UpdateResearch()
+    {
+
+    }
+}
